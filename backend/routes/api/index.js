d00ce0a23eba8f9dc734f9b9816239330b201502
FrontLineCoding <andrew.parks@tutanota.com>
2023-04-09 20:48:28 -0500

    Update on landing page

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,7 +3,9 @@ const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 const { restoreUser } = require('../../utils/auth.js');
 const { requireAuth } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js')
 
 router.use(restoreUser); //<---- ME FIRST
+router.use('/session', sessionRouter);
 
 module.exports = router;
